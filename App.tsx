49355dc2a6271d04451da34df3f602f28f3c2793
DeyDebasish2003 <04debasish03@gmail.com>
2026-01-12 17:38:36 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,105 @@
+import Acceptable from "./components/Acceptable";
+import Acceptableele from "./components/Acceptableele";
+import Billing from "./components/Billing";
+import Billingele from "./components/Billingele";
+import Header from "./components/Header";
+import Ild from "./components/Ildbanner";
+import Bantopic from "./components/Ildbannertopics";
+import Mobilenet from "./components/Mobilenet";
+import Mobilenettopics from "./components/Mobilenettopics";
+import Navbar from "./components/Navbar";
+import Retendation from "./components/Retentation";
+import Retentationtopics from "./components/Retententiontopic";
+import Smallban from "./components/Smallban";
+import Subheading from "./components/Subheading";
+import Termsandcondition from "./components/Termsandcondition";
+import Termscond from "./components/Termscond";
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+     
+  <Header/> 
+  <br />
+  <Subheading/>
+  <br />
+<Smallban/>
+
+  <br />
+<Ild/>
+<br />
+
+<Bantopic/>
+<br />
+
+<Retendation/>
+<br />
+
+<Retentationtopics/>
+
+
+<br />
+
+<Mobilenet/>
+
+
+<br />
+
+
+<Mobilenettopics/>
+
+
+<br />
+
+
+<Acceptable/>
+
+
+<br />
+ 
+ <Acceptableele/>
+
+
+ <br />
+
+
+
+ <Billing/>
+ 
+ 
+ 
+ 
+ <br />
+ 
+ 
+ <Billingele/>
+ 
+
+<br />
+<Navbar/>
+<Termsandcondition/>
+
+
+<br />
+<Termscond/>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+  );
+}
+
+export default App;
